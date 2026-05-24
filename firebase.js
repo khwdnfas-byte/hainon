3abe01853ba32db486c4c5026690ee17b41dce44
khwdnfas-byte <khwdnfas@gmail.com>
2026-05-24 05:40:51 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,17 +1,18 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
 const firebaseConfig = {
-apiKey: "AIzaSyB8qhLL9i-HXre2DpbJymf69GXAhs-J0cA",
-authDomain: "hainon-app-266f1.firebaseapp.com",
-projectId: "hainon-app-266f1",
-storageBucket: "hainon-app-266f1.firebasestorage.app",
-messagingSenderId: "154652777949",
-appId: "1:154652777949:web:2c716489ad05bae12faa34"
+    apiKey: "AIzaSyB8qhLL9i-HXre2DpbJymf69GXAhs-J0cA",
+    authDomain: "hainon-app-266f1.firebaseapp.com",
+    projectId: "hainon-app-266f1",
+    storageBucket: "hainon-app-266f1.firebasestorage.app",
+    messagingSenderId: "154652777949",
+    appId: "1:154652777949:web:2c716489ad05bae12faa34"
 };
 
+// تهيئة التطبيق والمكتبات
 const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
 export const db = getFirestore(app);
