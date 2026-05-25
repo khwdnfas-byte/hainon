f3b4125ae68023fb1bf7502df30f23d66bf0debc
khwdnfas-byte <khwdnfas@gmail.com>
2026-05-25 13:24:29 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,26 +1,24 @@
-// firebase.js - إعدادات Firebase لمشروع HAINON
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// =============================================
-// المفاتيح الحقيقية لمشروع HAINON
-// =============================================
-const firebaseConfig = {
-  apiKey: "AIzaSyB5VEhL-h84y4vzGuQRTTZra93DWLTyap4",
-  authDomain: "hainon-7c27d.firebaseapp.com",
-  databaseURL: "https://hainon-7c27d-default-rtdb.firebaseio.com",
-  projectId: "hainon-7c27d",
-  storageBucket: "hainon-7c27d.firebasestorage.app",
-  messagingSenderId: "240531479138",
-  appId: "1:240531479138:web:7e7a7d2788440728fd0f3b",
-  measurementId: "G-RYFVGRCYWP"
-};
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyB5VEhL-h84y4vzGuQRTTZra93DWLTyap4",
+    authDomain: "hainon-7c27d.firebaseapp.com",
+    databaseURL: "https://hainon-7c27d-default-rtdb.firebaseio.com",
+    projectId: "hainon-7c27d",
+    storageBucket: "hainon-7c27d.firebasestorage.app",
+    messagingSenderId: "240531479138",
+    appId: "1:240531479138:web:7e7a7d2788440728fd0f3b",
+    measurementId: "G-RYFVGRCYWP"
+  };
 
-// تهيئة Firebase
-const app = initializeApp(firebaseConfig);
-
-// تصدير خدمات المصادقة وقاعدة البيانات
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export default app;
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
